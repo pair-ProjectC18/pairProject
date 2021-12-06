c0830d11e4842192dc48ca3bb39307e6644787d9
ZERO111 <mouradchniti9@gmail.com>
2021-12-06 14:04:45 +0100

    our store

---
+++ main.js
@@ -0,0 +1,21 @@
+var $body = $("body")
+$body.append("<h1 id=title>GYM  Shop</h1>");
+$("#title ").css({
+
+    "width": "500 px ",
+    "margin ": "auto ",
+    "background ": "gray ",
+    //"text-align": "center",
+    // "color": "red",
+    ///"background-color": "lightgrey",
+    //"width": "300 px ",
+    // "border": "solid green ",
+    // "padding": "50 px ",
+    // "margin": "20 px"
+})
+$body.css({
+    "background-image": "url('./70-707080_dwayne-johnson-gym-the-rock-bodybuilder.jpg')",
+    "background-size": " cover ",
+    "background - position - y": "inherit ",
+    "background-repeat": "no-repeat"
+})
